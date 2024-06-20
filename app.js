f42a4a8a105c3de0fc0a98535a4e81b9a36a58e2
G0BI <85911423+G0BI@users.noreply.github.com>
2024-06-20 13:54:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/*server.js*/
+
+const http=require('http');
+const ip= '127.0.0.1';
+const port='3000';
+const server=http.createServer((req,res)=>{
+    res.write("Hello node");
+    res.end();
+});
+
+server.listen(port, ip,()=>{
+    console.log('server running at http://${ip}:${port}/');
+});
